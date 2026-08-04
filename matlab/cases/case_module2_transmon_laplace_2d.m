28fbc67e5e618736c1253a4971bccf0f66d1d3d5
LukeKim <kimchanwook3@gmail.com>
2026-08-03 22:21:50 -0400

    Add files via upload

---
+++ matlab/cases/case_module2_transmon_laplace_2d.m
@@ -0,0 +1,4 @@
+function params = case_module2_transmon_laplace_2d()
+% CASE_MODULE2_TRANSMON_LAPLACE_2D Tagged-electrode Module 9 Laplace case.
+params = default_module2_params('transmon_laplace');
+end
---
+++ matlab/cases/case_module2_transmon_trapped_charge_2d.m
@@ -0,0 +1,4 @@
+function params = case_module2_transmon_trapped_charge_2d()
+% CASE_MODULE2_TRANSMON_TRAPPED_CHARGE_2D Grounded transmon defect-charge case.
+params = default_module2_params('transmon_trapped_charge');
+end
